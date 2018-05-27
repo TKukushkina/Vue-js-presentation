589330093229c524dd38d5d4927296e050fb2d10
Tanya Kukushkina <kukushkina95@gmail.com>
2018-05-27 14:38:00 +0300

    added directives

--- src/components/Directives.vue
+++ src/components/Directives.vue
@@ -1,4 +1,28 @@
 <template>
-  <div class="title">Directives</div>
-  
+  <div class="directives h-100">
+    <div class="title">Directives</div>
+    <p class="m-auto" v-html='text'></p>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "<span class='bold'>Directives</span> are instruction for VueJS to do things in a certain way. There are a lot of directives such as v-if, v-show, v-else, v-for, v-bind , v-model, v-on, etc."
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .directives {
+    display: flex;
+    flex-direction: column;
+
+    p {
+      max-width: 600px;
+      font-size: 28px;
+    }
+  }
+</style>
